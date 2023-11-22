830359ad1f3a93200aecbd84e0a73e8674683630
ubaid0309 <ubaidskms@gmail.com>
2023-11-22 21:37:58 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        roboto : ['Roboto', 'sans-serif'],
+      },
+      colors:
+      {
+        "bgblack" : "#121212",
+        "staryellow" : "#FAAF00",
+        "skyblue" : "#526E85",
+        
+      }
+    },
+  },
+  plugins: [],
+}
