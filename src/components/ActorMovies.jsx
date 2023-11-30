4e2a679c9829d233869c88d4ede07531213225fa
ubaid0309 <ubaidskms@gmail.com>
2023-11-30 23:16:33 +0530

    setup for development build

--- src/components/ActorMovies.jsx
+++ src/components/ActorMovies.jsx
@@ -4,7 +4,7 @@ import MovieCard from './MovieCard';
 
 function ActorMovies({ actorId }) {
   const { data } = useGetActorMoviesQuery(actorId);
-  console.log(data);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-4">
       {data?.cast.map((movie) => (
